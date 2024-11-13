eb3e6ccf72d931c0776fce5c0e4495ff2b149603
stenOskar <stenhalse@gmail.com>
2024-11-13 02:42:18 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
-import Header from './Header';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test('renders header with title', () => {
-  render(<Header />);
-  const headerElement = screen.getByText(/Your Header Title/i);
-  expect(headerElement).toBeInTheDocument();
 });
