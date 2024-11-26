5370f5ad41b0969696f7c6741877414abf922534
stenOskar <stenhalse@gmail.com>
2024-11-26 17:03:40 +0100

    Implementing post-deployment smoke test

---
+++ scripts/smoke-test.sh
@@ -0,0 +1,16 @@
+# scripts/smoke-test.sh
+#!/bin/bash
+
+# Replace 'your-app-url' with the actual URL where your app is deployed
+APP_URL="http://35.228.30.185/"
+
+echo "Performing smoke test on $APP_URL"
+
+STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null "$APP_URL")
+
+if [[ "$STATUS_CODE" -ne 200 ]] ; then
+  echo "Smoke test failed: Expected 200 OK, but got $STATUS_CODE"
+  exit 1
+else
+  echo "Smoke test passed: Received 200 OK"
+fi
